1d5064470e6d29c12eabb82ae599b3f12bbb9b54
Gabriel <gabdasilva96@gmail.com>
2023-10-12 21:15:49 +0100

    update snap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-primary">
-
+    <main >
+      <section className={cn('bg-slate-600')}>Hero</section>
+      <section className={cn('bg-black')}>Products</section>
+      <section className={cn('bg-red-600')}>Features</section>
+      <section className={cn('bg-muted')}>Testimonials</section>
     </main>
   )
 }
