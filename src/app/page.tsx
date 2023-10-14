6f80e379d1ef091ed8abb198f4b5d492d600ecc6
Gabriel <gabdasilva96@gmail.com>
2023-10-14 14:57:58 +0100

    Added Nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ import { cn } from '@/lib/utils'
 export default function Home() {
   return (
     <main>
-      <section className={cn('bg-slate-600 ')}>Hero</section>
-      <section className={cn('bg-red-700')}>Products</section>
-      <section className={cn('bg-blue-700')}>About</section>
+
     </main>
   )
 }
---
+++ src/config/siteConfig.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "Silva Startups",
+  url: "",
+  description:
+    "",
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn-ui/ui",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
